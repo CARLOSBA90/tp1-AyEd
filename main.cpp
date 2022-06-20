7d2dc261a6eaba16f30c688a65851cc891595b79
Carlos Peña <CEPR0190@GMAIL.COM>
2022-06-20 15:48:05 -0300

    funcion ProcVentas

--- main.cpp
+++ main.cpp
@@ -10,62 +10,110 @@
 // Parametros de configuracion
 using namespace std;
 #define NOMBRE_ARCHIVO  "VentasFerreteria.Txt"
-struct vendedor {
-  string nombre;
-  // otros;
-};
-struct fecha {
-  string anio;
-  string mes;
-  string dia;
-};
+ifstream VentasAF (NOMBRE_ARCHIVO);
+const int MAX_REGS = 800;
+const int MAX_VENS = 100;
+int cantVtas = 0;
+string line = "";
 
-struct venta {
+struct sVendedor {
   int cod_ven;
   int cant;
   string descrip;
   double precio;
-  struct fecha;
-};
+  struct {
+        int dia;
+        int mes;
+        int anio;
+      }fecha;
+} rVendedor;
+vector<sVendedor> vrVendedores;
 
-const int MAX_REGS = 800;
-const int MAX_VENS = 100;
 
-int cantVtas = 0;
-string line;
-void ProcVentas(ifstream VentasAF,vector<vendedor> vrVendedores,int cantVtas);
+//------------------------------------------------------------------------------
+// Funciones
+void ProcVentas(ifstream &VentasAF,vector<sVendedor> &vrVendedores,int &cantVtas);
+boolean LeeUnaVta(ifstream &VentasAF,sVendedor &rVendedor);
 //void ListadoCantTot(SalidaAF, vrTotales,int cantVen);
 //void ListadoImpTot(SalidaAF, vrTotales,int cantVen);
-vector<venta> vrVtas;
-vector<vendedor> vrVendedores;
+//------------------------------------------------------------------------------
+
 
 
 int main(){
-fstream ventasAF(NOMBRE_ARCHIVO);
-
-cout<<"prueba de salida";
-
-  //ProcVentas(VentasAF,vrVendedores,cantVtas);
+    ProcVentas(VentasAF,vrVendedores,cantVtas);
   //ListadoVtasAgrupVen(SalidaAF,vrVendedores,vrTotales);
   //ListadoCantTot(SalidaAF,vrTotales,cantVen);
   //ListadoImpTot(SalidaAF,vrTotales,cantVen);
-  //  VentasAF.close();
+    VentasAF.close();
   //SalidaAF.close();
+ 
+ //------------------------------------------------------------------------------
+
+  // Pruebas de salida
+  for(int i=0; i<vrVendedores.size(); i++){
+          cout << "Cod_vend:  " << vrVendedores[i].cod_ven;
+           cout << "- Cant:  " << vrVendedores[i].cant;
+           cout << "- Descrip:  " << vrVendedores[i].descrip;
+           cout << "- Precio:  " << vrVendedores[i].precio;
+           cout << "- Fecha:  " << vrVendedores[i].fecha.dia << "/"<< vrVendedores[i].fecha.mes<< "/"<< vrVendedores[i].fecha.anio << endl;
+  }
+   cout << "ejecucion final" << endl;
+  //
   return 0;
 }
 
-void ProcVentas(ifstream VentasAF,vector<vendedor> vrVendedores,int  cantVtas){
+void ProcVentas(ifstream &VentasAF,vector<sVendedor> &vrVendedores,int  &cantVtas){
 
-/*
-LeeUnaVta(VentasAF, rVendedor), el cual lee una componente de datos del archivo y los almacena en una estructura interna. La funcion retorna un boolean, verdadero, si la lectura fue exitosa, caso contrario, falso.
-ProcVentas(VtasAF, vrVtas, cVtas) //Carga c/u. de los datos leidos en vrVtas. Se debe invocar a la funcion LeeUnaVta. Devuelve la cantidad de registros leidos en cVtas.
-OrdxBur(vrVtas, card); // Ord. x codigo de vendedor creciente.
-OrdxBur(vrTot, card); //Ord. x cantidad total de cada vendedor decrec.
-OrdxBur(card, vrTot); //Ord. x importe total de cada vendedor decrec.
-IntCmb(vrVtas[i], vrVtas[i+1]); //Debe invocarse dentro de la funcion OrdxBur.
-IntCmb(vrTot[i], vrTot[i+1]);    //Debe invocarse dentro de la funcion OrdxBur.
+  if (VentasAF.is_open()){
+     while(LeeUnaVta(VentasAF, rVendedor)){
+          ++cantVtas;
+         if(MAX_REGS<cantVtas){
+            cout << "CANTIDAD DE REGISTROS EXCEDIDO, DEBE SER MENOR A: "<< MAX_REGS << endl;
+              break;
+            }
+          vrVendedores.push_back(rVendedor);
+   }
+    
+  }else{ cout << "NO SE PUDO ABRIR EL ARCHIVO"; }
 
-*/
 
 }
 
+boolean LeeUnaVta(ifstream &VentasAF,sVendedor &rVendedor){
+string dato;
+boolean fin = false;
+
+/// conversion de tipos
+VentasAF >> dato;
+if(dato.empty()) 
+{ fin = true; }
+else{
+  try{
+      rVendedor.cod_ven = stoi(dato);
+
+      VentasAF >> dato;
+      int cantidad = 
+      rVendedor.cant = stoi(dato);
+
+      VentasAF >> dato;
+      rVendedor.descrip = dato;
+
+      VentasAF >> dato;
+      rVendedor.precio = stod(dato);
+
+      VentasAF >> dato;
+      rVendedor.fecha.dia = stoi(dato);
+
+      VentasAF >> dato;
+      rVendedor.fecha.mes = stoi(dato);
+
+      VentasAF >> dato;
+      rVendedor.fecha.anio = stoi(dato);
+      }
+   catch (...) {
+      cout << "ERROR EN LECTURA DE DATOS Y/O FORMATO INVALIDO.\n";
+    }   
+  }
+  return !fin;
+}
